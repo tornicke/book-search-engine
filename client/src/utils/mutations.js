2157d82162268e75dfbb4fe85f968d87f205707c
tornicke <torniqe@gmail.com>
2022-11-02 22:52:39 +0100

    Defining mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,50 @@
+import { gql } from "@apollo/client";
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook(
+    $bookId: ID!
+    $authors: [String]
+    $title: String
+    $description: String
+    $image: String
+    $link: String
+  ) {
+    saveBook(bookId: $bookID, authors: $authors) {
+      _id
+      user
+    }
+  }
+`;
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      user
+    }
+  }
+`;
